28543cd7f2ca8779a32db815d7f5efd429e631ec
Christian Alcalde <cc.alcalde@yahoo.com>
2022-04-20 11:53:26 -0700

    added docstring for sqlForPartialUpdate and wrote unit tests

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,14 +1,31 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Takes in dataToUpdate and jsToSql and returns a string of setCols and
+ * an array of values to pass into an SQL query.
+ *
+ * dataToUpdate = {firstName: 'Aliya', age: 32}
+ *
+ * This converts JS variable names to SQL column names:
+ *
+ * jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    }
+
+    Returns a joined string of columns and parameterized queries:
+
+      setCols: `"first_name"=$1, "age"=$2`
+      values: ['Aliya', 32]
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
---
+++ helpers/sql.test.js
@@ -0,0 +1,30 @@
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+const { set } = require("express/lib/application");
+
+describe("sql partial update", function () {
+  test("Fail: No data to update", function () {
+    const data = {};
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+
+    expect(() => sqlForPartialUpdate(data, jsToSql)).toThrow(BadRequestError);
+  });
+
+  test("Pass: receiving sql for update", function () {
+    const data = { firstName: "Test", age: 18 };
+    const jsToSql = {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    };
+
+    const { setCols, values } = sqlForPartialUpdate(data, jsToSql);
+
+    expect(setCols).toEqual(`"first_name"=$1, "age"=$2`);
+    expect(values).toEqual(["Test", 18]);
+  });
+});
