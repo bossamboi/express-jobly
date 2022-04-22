a239404b41da017bf8b53874b62f563e91ac5c1c
Eric Sohng <ericsohng@gmail.com>
2022-04-22 12:11:06 -0700

    write tests for apply

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const db = require("../db");
 const bcrypt = require("bcrypt");
 const { sqlForPartialUpdate } = require("../helpers/sql");
 const {
-  NotFoundError,
-  BadRequestError,
-  UnauthorizedError,
+	NotFoundError,
+	BadRequestError,
+	UnauthorizedError,
 } = require("../expressError");
 
 const { BCRYPT_WORK_FACTOR } = require("../config.js");
@@ -14,17 +14,17 @@ const { BCRYPT_WORK_FACTOR } = require("../config.js");
 /** Related functions for users. */
 
 class User {
-  /** authenticate user with username, password.
-   *
-   * Returns { username, first_name, last_name, email, is_admin }
-   *
-   * Throws UnauthorizedError is user not found or wrong password.
-   **/
+	/** authenticate user with username, password.
+	 *
+	 * Returns { username, first_name, last_name, email, is_admin }
+	 *
+	 * Throws UnauthorizedError is user not found or wrong password.
+	 **/
 
-  static async authenticate(username, password) {
-    // try to find the user first
-    const result = await db.query(
-      `SELECT username,
+	static async authenticate(username, password) {
+		// try to find the user first
+		const result = await db.query(
+			`SELECT username,
                   password,
                   first_name AS "firstName",
                   last_name AS "lastName",
@@ -32,53 +32,53 @@ class User {
                   is_admin AS "isAdmin"
            FROM users
            WHERE username = $1`,
-      [username]
-    );
+			[username]
+		);
 
-    const user = result.rows[0];
+		const user = result.rows[0];
 
-    if (user) {
-      // compare hashed password to a new hash from password
-      const isValid = await bcrypt.compare(password, user.password);
-      if (isValid === true) {
-        delete user.password;
-        return user;
-      }
-    }
+		if (user) {
+			// compare hashed password to a new hash from password
+			const isValid = await bcrypt.compare(password, user.password);
+			if (isValid === true) {
+				delete user.password;
+				return user;
+			}
+		}
 
-    throw new UnauthorizedError("Invalid username/password");
-  }
+		throw new UnauthorizedError("Invalid username/password");
+	}
 
-  /** Register user with data.
-   *
-   * Returns { username, firstName, lastName, email, isAdmin }
-   *
-   * Throws BadRequestError on duplicates.
-   **/
+	/** Register user with data.
+	 *
+	 * Returns { username, firstName, lastName, email, isAdmin }
+	 *
+	 * Throws BadRequestError on duplicates.
+	 **/
 
-  static async register({
-    username,
-    password,
-    firstName,
-    lastName,
-    email,
-    isAdmin,
-  }) {
-    const duplicateCheck = await db.query(
-      `SELECT username
+	static async register({
+		username,
+		password,
+		firstName,
+		lastName,
+		email,
+		isAdmin,
+	}) {
+		const duplicateCheck = await db.query(
+			`SELECT username
            FROM users
            WHERE username = $1`,
-      [username]
-    );
+			[username]
+		);
 
-    if (duplicateCheck.rows[0]) {
-      throw new BadRequestError(`Duplicate username: ${username}`);
-    }
+		if (duplicateCheck.rows[0]) {
+			throw new BadRequestError(`Duplicate username: ${username}`);
+		}
 
-    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
-    const result = await db.query(
-      `INSERT INTO users
+		const result = await db.query(
+			`INSERT INTO users
            (username,
             password,
             first_name,
@@ -87,101 +87,102 @@ class User {
             is_admin)
            VALUES ($1, $2, $3, $4, $5, $6)
            RETURNING username, first_name AS "firstName", last_name AS "lastName", email, is_admin AS "isAdmin"`,
-      [username, hashedPassword, firstName, lastName, email, isAdmin]
-    );
+			[username, hashedPassword, firstName, lastName, email, isAdmin]
+		);
 
-    const user = result.rows[0];
+		const user = result.rows[0];
 
-    return user;
-  }
+		return user;
+	}
 
-  /** Find all users.
-   *
-   * Returns [{ username, first_name, last_name, email, is_admin }, ...]
-   **/
+	/** Find all users.
+	 *
+	 * Returns [{ username, first_name, last_name, email, is_admin }, ...]
+	 **/
 
-  static async findAll() {
-    const result = await db.query(
-      `SELECT username,
+	static async findAll() {
+		const result = await db.query(
+			`SELECT username,
                   first_name AS "firstName",
                   last_name AS "lastName",
                   email,
                   is_admin AS "isAdmin"
            FROM users
            ORDER BY username`
-    );
+		);
 
-    return result.rows;
-  }
+		return result.rows;
+	}
 
-  /** Given a username, return data about user.
-   *
-   * Returns { username, first_name, last_name, is_admin, jobs }
-   *   where jobs is { id, title, company_handle, company_name, state }
-   *
-   * Throws NotFoundError if user not found.
-   **/
+	/** Given a username, return data about user.
+	 *
+	 * Returns { username, first_name, last_name, is_admin, jobs }
+	 *   where jobs is { id, title, company_handle, company_name, state }
+	 *
+	 * Throws NotFoundError if user not found.
+	 **/
 
-  static async get(username) {
-    const userRes = await db.query(
-      `SELECT username,
+	static async get(username) {
+		const userRes = await db.query(
+			`SELECT username,
                   first_name AS "firstName",
                   last_name AS "lastName",
                   email,
                   is_admin AS "isAdmin"
            FROM users
            WHERE username = $1`,
-      [username]
-    );
+			[username]
+		);
 
-    const user = userRes.rows[0];
+		const user = userRes.rows[0];
 
-    if (!user) throw new NotFoundError(`No user: ${username}`);
+		if (!user) throw new NotFoundError(`No user: ${username}`);
 
-    const jobRes = await db.query(
-      `SELECT job_id AS jobId,
+		const jobRes = await db.query(
+			`SELECT job_id AS "jobId"
 			  FROM applications
 			  WHERE username = $1`,
-      [username]
-    );
+			[username]
+		);
 
-    const jobs = jobRes.rows;
+		console.log(jobRes.rows);
+		const jobs = jobRes.rows.map((r) => r.jobId);
 
-    user.jobs = jobs;
+		user.jobs = jobs;
 
-    return user;
-  }
+		return user;
+	}
 
-  /** Update user data with `data`.
-   *
-   * This is a "partial update" --- it's fine if data doesn't contain
-   * all the fields; this only changes provided ones.
-   *
-   * Data can include:
-   *   { firstName, lastName, password, email, isAdmin }
-   *
-   * Returns { username, firstName, lastName, email, isAdmin }
-   *
-   * Throws NotFoundError if not found.
-   *
-   * WARNING: this function can set a new password or make a user an admin.
-   * Callers of this function must be certain they have validated inputs to this
-   * or a serious security risks are opened.
-   */
+	/** Update user data with `data`.
+	 *
+	 * This is a "partial update" --- it's fine if data doesn't contain
+	 * all the fields; this only changes provided ones.
+	 *
+	 * Data can include:
+	 *   { firstName, lastName, password, email, isAdmin }
+	 *
+	 * Returns { username, firstName, lastName, email, isAdmin }
+	 *
+	 * Throws NotFoundError if not found.
+	 *
+	 * WARNING: this function can set a new password or make a user an admin.
+	 * Callers of this function must be certain they have validated inputs to this
+	 * or a serious security risks are opened.
+	 */
 
-  static async update(username, data) {
-    if (data.password) {
-      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
-    }
+	static async update(username, data) {
+		if (data.password) {
+			data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+		}
 
-    const { setCols, values } = sqlForPartialUpdate(data, {
-      firstName: "first_name",
-      lastName: "last_name",
-      isAdmin: "is_admin",
-    });
-    const usernameVarIdx = "$" + (values.length + 1);
+		const { setCols, values } = sqlForPartialUpdate(data, {
+			firstName: "first_name",
+			lastName: "last_name",
+			isAdmin: "is_admin",
+		});
+		const usernameVarIdx = "$" + (values.length + 1);
 
-    const querySql = `UPDATE users
+		const querySql = `UPDATE users
                       SET ${setCols}
                       WHERE username = ${usernameVarIdx}
                       RETURNING username,
@@ -189,49 +190,68 @@ class User {
                                 last_name AS "lastName",
                                 email,
                                 is_admin AS "isAdmin"`;
-    const result = await db.query(querySql, [...values, username]);
-    const user = result.rows[0];
+		const result = await db.query(querySql, [...values, username]);
+		const user = result.rows[0];
 
-    if (!user) throw new NotFoundError(`No user: ${username}`);
+		if (!user) throw new NotFoundError(`No user: ${username}`);
 
-    delete user.password;
-    return user;
-  }
+		delete user.password;
+		return user;
+	}
 
-  /** Delete given user from database; returns undefined. */
+	/** Delete given user from database; returns undefined. */
 
-  static async remove(username) {
-    let result = await db.query(
-      `DELETE
+	static async remove(username) {
+		let result = await db.query(
+			`DELETE
            FROM users
            WHERE username = $1
            RETURNING username`,
-      [username]
-    );
-    const user = result.rows[0];
+			[username]
+		);
+		const user = result.rows[0];
 
-    if (!user) throw new NotFoundError(`No user: ${username}`);
-  }
+		if (!user) throw new NotFoundError(`No user: ${username}`);
+	}
 
-  /** Allows users to apply for a job via Application joiner table.
-   *
-   * Returns {username, jobId}
-   */
+	/** Allows users to apply for a job via Application joiner table.
+	 *
+	 * Returns {username, jobId}
+	 *
+	 * Returns NotFoundError if username or id is not valid
+	 */
 
-  static async apply(username, id) {
-    let result = await db.query(
-      `
+	static async apply(username, id) {
+		let userRes = await db.query(
+			`SELECT username
+        FROM users
+        WHERE username = $1`,
+			[username]
+		);
+		if (!userRes.rows[0]) throw new NotFoundError(`No user: ${username}`);
+
+		let jobRes = await db.query(
+			`SELECT title
+        FROM jobs
+        WHERE id = $1`,
+			[id]
+		);
+		if (!jobRes.rows[0]) throw new NotFoundError(`No job: ${id}`);
+
+		let result = await db.query(
+			`
       INSERT INTO applications
         (username,
           job_id)
        VALUES ($1, $2)
-       RETURNING username, job_id AS jobId`,
-      [username, id]
-    );
+       RETURNING username, job_id AS "jobId"`,
+			[username, id]
+		);
 
-    let application = result.rows[0];
-    return application;
-  }
+		let application = result.rows[0];
+
+		return application;
+	}
 }
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,9 @@ const jsonschema = require("jsonschema");
 
 const express = require("express");
 const {
-  ensureLoggedIn,
-  ensureIsAdmin,
-  ensureCorrectUserOrAdmin,
+	ensureLoggedIn,
+	ensureIsAdmin,
+	ensureCorrectUserOrAdmin,
 } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
@@ -32,15 +32,15 @@ const router = express.Router();
  **/
 
 router.post("/", ensureIsAdmin, async function (req, res, next) {
-  const validator = jsonschema.validate(req.body, userNewSchema);
-  if (!validator.valid) {
-    const errs = validator.errors.map((e) => e.stack);
-    throw new BadRequestError(errs);
-  }
+	const validator = jsonschema.validate(req.body, userNewSchema);
+	if (!validator.valid) {
+		const errs = validator.errors.map((e) => e.stack);
+		throw new BadRequestError(errs);
+	}
 
-  const user = await User.register(req.body);
-  const token = createToken(user);
-  return res.status(201).json({ user, token });
+	const user = await User.register(req.body);
+	const token = createToken(user);
+	return res.status(201).json({ user, token });
 });
 
 /** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
@@ -51,8 +51,8 @@ router.post("/", ensureIsAdmin, async function (req, res, next) {
  **/
 
 router.get("/", ensureIsAdmin, async function (req, res, next) {
-  const users = await User.findAll();
-  return res.json({ users });
+	const users = await User.findAll();
+	return res.json({ users });
 });
 
 /** GET /[username] => { user }
@@ -63,12 +63,12 @@ router.get("/", ensureIsAdmin, async function (req, res, next) {
  **/
 
 router.get(
-  "/:username",
-  ensureCorrectUserOrAdmin,
-  async function (req, res, next) {
-    const user = await User.get(req.params.username);
-    return res.json({ user });
-  }
+	"/:username",
+	ensureCorrectUserOrAdmin,
+	async function (req, res, next) {
+		const user = await User.get(req.params.username);
+		return res.json({ user });
+	}
 );
 
 /** PATCH /[username] { user } => { user }
@@ -82,18 +82,18 @@ router.get(
  **/
 
 router.patch(
-  "/:username",
-  ensureCorrectUserOrAdmin,
-  async function (req, res, next) {
-    const validator = jsonschema.validate(req.body, userUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map((e) => e.stack);
-      throw new BadRequestError(errs);
-    }
+	"/:username",
+	ensureCorrectUserOrAdmin,
+	async function (req, res, next) {
+		const validator = jsonschema.validate(req.body, userUpdateSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map((e) => e.stack);
+			throw new BadRequestError(errs);
+		}
 
-    const user = await User.update(req.params.username, req.body);
-    return res.json({ user });
-  }
+		const user = await User.update(req.params.username, req.body);
+		return res.json({ user });
+	}
 );
 
 /** DELETE /[username]  =>  { deleted: username }
@@ -102,29 +102,31 @@ router.patch(
  **/
 
 router.delete(
-  "/:username",
-  ensureCorrectUserOrAdmin,
-  async function (req, res, next) {
-    await User.remove(req.params.username);
-    return res.json({ deleted: req.params.username });
-  }
+	"/:username",
+	ensureCorrectUserOrAdmin,
+	async function (req, res, next) {
+		await User.remove(req.params.username);
+		return res.json({ deleted: req.params.username });
+	}
 );
 
 /** POST /[username]/jobs/[id]  =>  { applied: jobId }
+ *
+ * NEW APPLICATION
  *
  * Authorization required: login as admin or own user
  **/
 
 router.post(
-  "/:username/jobs/:id",
-  ensureCorrectUserOrAdmin,
-  async function (req, res, next) {
-    const username = req.params.username;
-    const id = req.params.id;
+	"/:username/jobs/:id",
+	ensureCorrectUserOrAdmin,
+	async function (req, res, next) {
+		const username = req.params.username;
+		const id = req.params.id;
 
-    const application = await User.apply(username, id);
-    return res.json({ applied: application.jobId });
-  }
+		const application = await User.apply(username, id);
+		return res.json({ applied: application.jobId });
+	}
 );
 
 module.exports = router;
